caaa5e994cdf0022bf0fc11fc77721f7ec94fe6b
Jordy1311 <jordancooper1195@gmail.com>
2021-04-15 14:33:55 +1200

    cubes dynamically generated by week & test event listeners on form-submit & weeks

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('testing')
+const contributionGraph = document.getElementById('year')
+const textForm = document.getElementById('text-form')
+const textField = document.getElementById('text-field')
+
+window .onload = function() {
+    for(i=0; i<53; i++) {
+        const week = document.createElement('div')
+        week.className = 'week'
+        week.innerHTML = '<div class="commit-1"></div><div class="commit-1"></div><div class="commit-1"></div><div class="commit-1"></div><div class="commit-1"></div><div class="commit-1"></div><div class="commit-1"></div>'
+        week.addEventListener('click', function() {console.log(`${week[i]} says: Ouch!!`)})
+        contributionGraph.appendChild(week)
+    }}
+
+textForm.addEventListener('submit', function(e) {
+    console.log(textField.value)
+    e.preventDefault()
+})
